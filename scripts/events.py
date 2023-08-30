9e85e31ef35f25137fabecde94dbbfe39d176cec
Marton Endredi <endredimarci@gmail.com>
2023-08-30 21:57:54 +0200

    event letöltés

---
+++ scripts/events.py
@@ -0,0 +1,15 @@
+import json
+import requests
+
+api_key = "EAAVGbiFIyVMBO4qGWeSZBvZAATIsngfwOlri3iW9GrDOxXaY2xb8F7AzEI1ynXVGKDpuZCcAuGBXDtQ9RscS79reyjXNHDflM1Ytdmt8yqb1qzFrvQ4kXvpAZAZAKrnncZC9oQinu66SVKhSZBizjE1jlxEhSAhycoVQthJNuA789wrTfREZAnteE0ILwdIMwZCUsZCgc34aM5U2j6lUgZD"
+
+URL = "https://graph.facebook.com/112998758567158?fields=events{name,description,start_time,attending_count,cover}&access_token=" + api_key
+response = requests.get(URL, stream=True)
+
+data = ""
+
+for c in response.iter_content():
+    data += c.decode()
+
+with open("events.json", "w+", encoding="utf-8") as file:
+    file.write(json.dumps(json.loads(data), indent=4))
