b20f3e28d517c6fa5985b3db5bfb885e66202e31
Marton Endredi <endredimarci@gmail.com>
2023-10-08 13:31:29 +0200

    magyarosítás

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,6 @@
 document.addEventListener('scroll', () => {
     if (window.scrollY > 0) {
-        document.querySelector('header').setAttribute('id', 'shrunk');
+        document.querySelector('header').setAttribute('id', 'kicsi');
     } else {
         document.querySelector('header').removeAttribute('id');
     }
